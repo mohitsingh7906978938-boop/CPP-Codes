4df7f11d57fbfc6c65083604a4e9ca877bd52e51
Mohit Singh <mohitsingh1234@gmail.com>
2025-12-25 12:54:12 +0530

    My First Code

---
+++ Hello World.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Library for input and output
+
+int main() {
+    // std::cout prints text to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0; // Signals that the program finished successfully
+}
